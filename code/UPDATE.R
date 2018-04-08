1c982505b5a16b878dd6183c423a805993cac9c6
majohansson <michael.johansson@gmail.com>
2018-04-08 11:08:04 -0400

    add code for calculating client numbers

--- code/UPDATE.R
+++ code/UPDATE.R
@@ -3,3 +3,11 @@ source('code/Electricity.R')
 source('code/StatusPR Trends.R')
 
 Sys.Date() - as.Date('2017-09-20')
+
+aee_clients = 1413000
+aee_clients * (1 - elec$Value[nrow(elec)] / 100)
+acueductos_clients = 1220996 / 0.9932
+pr_water_prop = status %>%
+  filter(Resource == 'Water', Location == 'Puerto Rico') %>%
+  arrange(desc(date))
+acueductos_clients * (1 - pr_water_prop$Value[1]/100)
