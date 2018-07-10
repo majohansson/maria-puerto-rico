236863d6352806b460e5f44e7b35b645d4438afc
majohansson <michael.johansson@gmail.com>
2018-07-10 17:15:57 -0400

    updated

--- code/UPDATE.R
+++ code/UPDATE.R
@@ -4,13 +4,13 @@ source('code/StatusPR Trends.R')
 
 Sys.Date() - as.Date('2017-09-20')
 
-aee_clients = 1413000
-clients <- filter(status, Resource == 'Clients with electricity') %>%
-  arrange(date)
-aee_clients * (1 - clients$Value[nrow(clients)] / 100)
-
-acueductos_clients = 1220996 / 0.9932
-pr_water_prop = status %>%
-  filter(Resource == 'Water', Location == 'Puerto Rico') %>%
-  arrange(desc(date))
-acueductos_clients * (1 - pr_water_prop$Value[1]/100)
+# aee_clients = 1413000
+# clients <- filter(status, Resource == 'Clients with electricity') %>%
+#   arrange(date)
+# aee_clients * (1 - clients$Value[nrow(clients)] / 100)
+# 
+# acueductos_clients = 1220996 / 0.9932
+# pr_water_prop = status %>%
+#   filter(Resource == 'Water', Location == 'Puerto Rico') %>%
+#   arrange(desc(date))
+# acueductos_clients * (1 - pr_water_prop$Value[1]/100)
